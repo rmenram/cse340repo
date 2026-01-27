a926b627e770ec121e4581f9818f22c3cc3c7820
rmenram <rmendozaramos@byupathway.edu>
2026-01-27 03:24:22 -0800

    week 3 assignment corrections

--- utilities/index.js
+++ utilities/index.js
@@ -38,9 +38,9 @@ Util.buildClassificationGrid = async function(data){
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
       + 'details"><img src="' + vehicle.inv_thumbnail 
       +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors" /></a>'
+      +' on CSE Motors"></a>'
       grid += '<div class="namePrice">'
-      grid += '<hr />'
+      grid += '<hr>'
       grid += '<h2>'
       grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
       + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
@@ -69,7 +69,7 @@ Util.buildVehicleGrid = async function(data){
       grid += '<div class="vehicle-photo">'
       grid +=  '<img src="' + vehicle.inv_image 
       +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors" />'
+      +' on CSE Motors">'
       grid += '</div>'
       grid += '<div class="vehicle-details">'
       grid += '<h2>'
@@ -93,6 +93,7 @@ Util.buildVehicleGrid = async function(data){
       grid += '<span class="field">Miles: </span>'
       grid += '<span>' 
       + new Intl.NumberFormat('en-US').format(vehicle.inv_miles) + '</span>'
+      grid += '</div>'
       grid += '</div>'
       grid += '</div>'
     })
