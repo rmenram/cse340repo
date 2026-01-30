f9807ce5d3c71c3ffbc8d48903ae988c12337365
rmenram <rmendozaramos@byupathway.edu>
2026-01-30 14:16:27 -0800

    week 4 activities

---
+++ routes/accountRoute.js
@@ -0,0 +1,20 @@
+// Needed Resources 
+const regValidate = require('../utilities/account-validation')
+const express = require("express")
+const router = new express.Router() 
+const accountController = require("../controllers/accountController");
+const utilities = require("../utilities/");
+
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const accountRoute = require("./routes/accountRoute")
 const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require('./database/')
+const bodyParser = require("body-parser")
 
 /* ***********************
  * View Engine and Templates
@@ -20,6 +24,30 @@ const utilities = require("./utilities/")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+ }))
+//Registration Activity
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) //for parsing application/ x-www-form-urlencoded
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * Routes
@@ -33,6 +61,8 @@ app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+// Account routes
+app.use("/account", accountRoute)
 // 500 Route 
 app.get('/500-error', (req, res, next) => {
   next({ status: 500, message: 'I am here for a special purpose.' })
